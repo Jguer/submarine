83cdb34fa20ff85899a85619717721d42445eee1
Jguer <johngr@tuta.io>
2015-08-04 16:56:45 +0100

    Removed accidental build

--- build_dir/po/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jguer/ownCloud/Projects/Vala/Submarine/submarine/po
-# Build directory: /home/jguer/ownCloud/Projects/Vala/Submarine/submarine/build_dir/po
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_dir/src/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jguer/ownCloud/Projects/Vala/Submarine/submarine/src/lib
-# Build directory: /home/jguer/ownCloud/Projects/Vala/Submarine/submarine/build_dir/src/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
